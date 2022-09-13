315c00b28dd6b11385f865ced63c5698926d35de
승빈 임 <imbi9437@gmail.com>
2022-09-13 21:27:54 +0900

    feat : interaction interface and singleton script added

---
+++ Assets/Script/Interface/IInteratable.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IInteratable
+{
+    public void Interaction();
+    public void OnFoucus();
+    public void OutFoucus();
+}
