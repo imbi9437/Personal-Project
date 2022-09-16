a6498588805ec4c383185effae1848243a0eca10
승빈 임 <imbi9437@gmail.com>
2022-09-16 21:47:16 +0900

    feat : zombie animation

--- Assets/Script/Zombie/Zombie.cs
+++ Assets/Script/Zombie/Zombie.cs
@@ -5,60 +5,25 @@ using UnityEngine;
 [RequireComponent (typeof(CharacterController),(typeof(Animator)))]
 public class Zombie : MonoBehaviour
 {
-    [SerializeField]
     private GameObject airplane;
-    public GameObject Airplane { get { return airplane; } }
 
-    public CharacterController characterController;
-    public Animator animator;
-    public StateMachine<States, Zombie> stateMachine;
+    private CharacterController characterController;
+    private Animator animator;
 
-    public enum States { Fall, Idle, Trace, Attack, Hit, Die }
-
-    private float maxHp = 300f;
-    public float MaxHP { get { return maxHp; } }
+    private float maxHp;
+    public float MaxHp { get { return maxHp; } set { maxHp = value; } }
     private float curHp;
     public float CurHp { get { return curHp; } set { curHp = value; } }
-    private float maxDamage = 20f;
-    public float MaxDamage { get { return maxDamage; } }
-
-    [SerializeField]
-    private LayerMask targetLayer;
-    public LayerMask TargetLayer { get { return targetLayer; } }
-    [SerializeField]
-    private GameObject target;
-    public GameObject Target { get { return target; } set { target = value; } }
-    [SerializeField, Range(0f, 300f)]
-    private float hp = 10f;
-    public float Hp { get { return hp; } set { hp = value; } }
-    [SerializeField, Range(0f, 10f)]
     private float speed;
     public float Speed { get { return speed; } set { speed = value; } }
+    private float hp;
+    public float Hp { get { return hp; } set { hp = value; } }
+    private float maxDamage;
+    public float MaxDamage { get { return maxDamage; } set { maxDamage = value; } }
 
     private void Awake()
     {
         characterController = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
-        stateMachine = new StateMachine<States, Zombie>(this);
-        stateMachine.AddState(States.Fall, new ZombieStates.Fall());
-        stateMachine.AddState(States.Idle, new ZombieStates.Idle());
-        stateMachine.AddState(States.Trace, new ZombieStates.Trace());
-        stateMachine.AddState(States.Attack, new ZombieStates.Attack());
-        stateMachine.AddState(States.Hit, new ZombieStates.Hit());
-        stateMachine.AddState(States.Die, new ZombieStates.Die());
-    }
-    private void OnEnable()
-    {
-        transform.parent = null;
-        stateMachine.ChangeState(States.Fall);
-    }
-    private void Update()
-    {
-        stateMachine.Update();
-    }
-
-    public void ChangeState(States nextState)
-    {
-        stateMachine.ChangeState(nextState);
     }
 }
